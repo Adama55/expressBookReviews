93516ed379828ba3a3e37c603cb9cc030b938100
Adama55 <mafahfof55@gmail.com>
2023-07-28 18:54:19 +0200

    final task

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,43 +26,60 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books, null, 2))
+
+  const getBooks = new Promise ((resolve, reject) => {
+    resolve(res.send(JSON.stringify(books, null, 2)))
+  })
+  
+  getBooks.then(()=> console.log(" Promise resolved for taske 10"))
   ;
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  res.send(books[isbn]) 
+  const getIbsn = new Promise ((resolve, reject) => {
+    const isbn = req.params.isbn;
+    resolve(res.send(books[isbn]))
+  })
+  getIbsn.then(() => console.log("promise for isbn has reselved, task 11"))
  });
 
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  const authorName = req.params.author;
-  const arrayBooks = [];
-  for ( const isbn in books) {
-    const book = books[isbn];
-    if (book.author === authorName) {
-      arrayBooks.push({isbn: isbn, title: book.title});
+  const getAuthor = new Promise ((resolve, reject) => {
+    const authorName = req.params.author;
+    const arrayBooks = [];
+    for ( const isbn in books) {
+      const book = books[isbn];
+      if (book.author === authorName) {
+        arrayBooks.push({isbn: isbn, title: book.title});
+      }
     }
-  }
-  return res.send(arrayBooks)
+    resolve(res.send(arrayBooks)).console.log(" auther found")
+  })
+  reject(console.log("author not find"))
+  
+  getAuthor.then(() => console.log(" promise resoved for Task 12"))
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  const titlePut = req.params.title;
-  const arrayBooks = [];
-  for ( const isbn in books) {
-    const book = books[isbn];
-    if (book.title === titlePut) {
-      arrayBooks.push({isbn: isbn, author: book.author});
+  const getTitle = new Promise((resolve, reject) => {
+    const titlePut = req.params.title;
+    const arrayBooks = [];
+    for ( const isbn in books) {
+      const book = books[isbn];
+      if (book.title === titlePut) {
+        arrayBooks.push({isbn: isbn, author: book.author});
+      }
     }
-  }
-  return res.send(arrayBooks)
- ;
+   resolve(res.send(arrayBooks))
+  })
+  reject(console.log("author not find"));
+  getTitle.then(() => console.log(" promise resoved for Task 13"))
+ 
 });
 
 //  Get book review
